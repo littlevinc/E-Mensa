261ba5cfde2aa70943de682420de6d079af23d95
Luke <luke.vincent.braun@gmail.com>
2022-11-17 18:23:24 +0100

    Update m3_aufgabe_6.sql

--- beispiele/m3/m3_aufgabe_6.sql
+++ beispiele/m3/m3_aufgabe_6.sql
@@ -1,3 +1,4 @@
+/*Aufgabge 1*/
 SELECT G.id, G.name, G.beschreibung, GROUP_CONCAT(H.code) AS "Allergen Code", GROUP_CONCAT(A.name) AS "Allergene"
 FROM gericht G 
 LEFT JOIN gericht_hat_allergen H ON G.id = H.gericht_id
@@ -5,34 +6,27 @@ LEFT JOIN allergen A On H.code = A.code
 GROUP BY id
 ORDER BY G.id;
 
-
-/* Alternative für a mit sub select*/
-SELECT id, name, beschreibung, (SELECT * FROM allergen WHERE code IN code)
-FROM gericht G LEFT JOIN gericht_hat_allergen H ON G.id = H.gericht_id
-GROUP BY id
-ORDER BY G.id;
-
-
-
-
-
+/*Aufgabge 2*/
 SELECT id, name, beschreibung, GROUP_CONCAT(code)
 FROM gericht G LEFT JOIN gericht_hat_allergen H ON G.id = H.gericht_id
 GROUP BY id
 ORDER BY G.id;
 
+/*Aufgabge 3*/
 SELECT *
 FROM gericht G 
 RIGHT JOIN gericht_hat_allergen H ON G.id = H.gericht_id
 RIGHT JOIN allergen A ON H.code = A.code
 ORDER BY id;
 
+/*Aufgabge 4*/
 SELECT A.name AS "Kategorie", COUNT(G.name) AS "Anzahl Gerichte"
 FROM gericht G
 JOIN gericht_hat_kategorie K ON G.id = K.gericht_id
 JOIN kategorie A ON A.id = K.kategorie_id
 GROUP BY A.name;
 
+/*Aufgabge 5*/
 SELECT A.name AS "Kategorie", COUNT(G.name) AS "Anzahl Gerichte"
 FROM gericht G
 JOIN gericht_hat_kategorie K ON G.id = K.gericht_id
