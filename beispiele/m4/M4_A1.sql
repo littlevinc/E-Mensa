687a6307c136455df5ccfc32123b08c2f5f26982
Luke <luke.vincent.braun@gmail.com>
2022-12-06 23:29:28 +0100

    Update M4_A1.sql

--- beispiele/m4/M4_A1.sql
+++ beispiele/m4/M4_A1.sql
@@ -1,11 +1,27 @@
 CREATE TABLE wunschgerichte (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    name VARCHAR(50), 
-    beschreibung VARCHAR(1000), 
-    erstelldatum DATE, 
-    ersteller VARCHAR(100), 
-    email VARCHAR(200)
-);
+    id_gericht INT PRIMARY KEY AUTO_INCREMENT,
+    gericht_name varchar(100) UNIQUE ,
+    beschreibung varchar(1000),
+    erstelldatum DATE,
+    ersteller INT,
+    FOREIGN KEY (ersteller) REFERENCES ersteller(id_ersteller)
+    );
+
+CREATE TABLE ersteller (
+    id_ersteller INT PRIMARY KEY AUTO_INCREMENT,
+    name varchar(100),
+    email varchar(255),
+    CONSTRAINT UNIQUE (name, email)
+    );
 
 
 
+/* 
+
+>> Relationsschreibweise
+
+Wunschgerichte: _ID_GERICHT_, 
+
+
+
+*/
